071294b520b211a00fd34f0b55037c06b31d9c00
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-06-22 00:29:19 +0530

    FinancialForecasting imp.

---
+++ Week_1/Algorithms_Data Structures/FinancialForecasting/FinancialForecasting/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FinancialForecasting
+{
+    using System;
+
+    class FinancialForecast
+    {
+        static double CalculateFutureValue(double initialValue, double growthRate, int years)
+        {
+            if (years==0)
+                return initialValue;
+
+            return CalculateFutureValue(initialValue, growthRate, years-1) * (1+growthRate);
+        }
+
+        static void Main(string[] args){
+            double initialValue = 10000;
+            double growthRate = 0.05;
+            int years = 10;
+
+            double futureValue = CalculateFutureValue(initialValue, growthRate, years);
+            Console.WriteLine($"Future Value after {years} years: {futureValue:C}");
+        }
+    }
+}
