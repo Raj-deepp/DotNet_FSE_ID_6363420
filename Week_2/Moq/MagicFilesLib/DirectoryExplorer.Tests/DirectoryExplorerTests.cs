6df4071b2197db3994e46b69dc7598ec18a5d7da
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-06-30 01:15:57 +0530

    DirecExp test comp.

---
+++ Week_2/Moq/MagicFilesLib/DirectoryExplorer.Tests/DirectoryExplorerTests.cs
@@ -0,0 +1,35 @@
+﻿using MagicFilesLib;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DirectoryExplorer.Tests
+{
+    [TestFixture]
+    public class DirectoryExplorerTests
+    {
+        private readonly string _file1 = "file.txt";
+        private readonly string _file2 = "file2.txt";
+        private IDirectoryExplorer _directoryExplorer;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            var mock = new Mock<IDirectoryExplorer>();
+            mock.Setup(de => de.GetFiles(It.IsAny<string>()))
+                .Returns(new List<string> { _file1, _file2 });
+
+            _directoryExplorer = mock.Object;
+        }
+
+        [Test]
+        public void GetFiles_ShouldReturnExpectedFiles()
+        {
+            var result = _directoryExplorer.GetFiles("dummyPath");
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result, Does.Contain(_file1));
+        }
+    }
+}
