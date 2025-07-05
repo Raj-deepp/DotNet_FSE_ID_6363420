b6146aa693781132cde46f57b4389fd026f921b7
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-06 02:09:53 +0530

    Data retrive from db

--- Week_3/Lab_1/RetailInventory/RetailInventory/Program.cs
+++ Week_3/Lab_1/RetailInventory/RetailInventory/Program.cs
@@ -1,17 +1,47 @@
-﻿using RetailInventory.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
 using RetailInventory.Models;
+
+
+//LAB 4 ->  INSERTING INITIAL DATA INTO THE DATABASE
+
+//var context = new AppDbContext();
+
+//var electronics = new Category { Name = "Electronics" };
+//var groceries = new Category { Name = "Groceries" };
+
+//await context.Categories.AddRangeAsync(electronics, groceries);
+
+//var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
+//var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
+
+//await context.Products.AddRangeAsync(product1, product2);
+//await context.SaveChangesAsync();
+
+//Console.WriteLine("Data inserted successfully.");
+
+//--------------------------------------------------------------------------------------
+
+
+
+//LAB 5 ->  RETRIRVING DATA FROM THE DATABASE
 
 var context = new AppDbContext();
 
-var electronics = new Category { Name = "Electronics" };
-var groceries = new Category { Name = "Groceries" };
+var products = await context.Products.ToListAsync();
+Console.WriteLine("All Products:");
+foreach (var p in products)
+    Console.WriteLine($"{p.Name} - ₹{p.Price}");
 
-await context.Categories.AddRangeAsync(electronics, groceries);
+Console.WriteLine();
 
-var product1 = new Product { Name = "Laptop", Price = 75000, Category = electronics };
-var product2 = new Product { Name = "Rice Bag", Price = 1200, Category = groceries };
+var product = await context.Products.FindAsync(1);
+Console.WriteLine($"Found (by ID 1): {product?.Name}");
 
-await context.Products.AddRangeAsync(product1, product2);
-await context.SaveChangesAsync();
+Console.WriteLine();
 
-Console.WriteLine("Data inserted successfully.");
+var expensive = await context.Products.FirstOrDefaultAsync(p => p.Price > 50000);
+Console.WriteLine($"Expensive product (> ₹50000): {expensive?.Name}");
+
+//--------------------------------------------------------------------------------------
+
