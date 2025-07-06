8933be307bfd8ee4c4560bc500a1f71aea40f1a8
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-06 20:45:07 +0530

    Migrationa & schema changes

--- Week_3/Lab_1/RetailInventory/RetailInventory/Migrations/20250706143545_AddStockQuantity.Designer.cs
+++ Week_3/Lab_1/RetailInventory/RetailInventory/Migrations/20250706143545_AddStockQuantity.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RetailInventory.Data;
 
@@ -10,9 +11,11 @@ using RetailInventory.Data;
 namespace RetailInventory.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250706143545_AddStockQuantity")]
+    partial class AddStockQuantity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
