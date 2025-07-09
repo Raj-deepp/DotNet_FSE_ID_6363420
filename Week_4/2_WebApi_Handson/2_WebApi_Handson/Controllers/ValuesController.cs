ee9e96bd143293afaceb3586c8b56f38eacbfe46
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-09 17:44:23 +0530

    Web Api using .Net core with Swagger

---
+++ Week_4/2_WebApi_Handson/2_WebApi_Handson/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2_WebApi_Handson.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+    }
+}
