317e4f6dfec28990e9bd2176520ed98a32732359
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-12 01:00:25 +0530

    Emp model and controller

---
+++ Week_4/5_WebApi_Handson/5_WebApi_Handson/Controllers/EmpController.cs
@@ -0,0 +1,25 @@
+﻿using _5_WebApi_Handson.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _5_WebApi_Handson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "POC,Admin")]
+    public class EmployeeController : ControllerBase
+    {
+        private static List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, Name = "Ravi", Department = "HR" },
+            new Employee { Id = 2, Name = "Sita", Department = "IT" }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_employees);
+        }
+    }
+}
+
---
+++ Week_4/5_WebApi_Handson/5_WebApi_Handson/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace _5_WebApi_Handson.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
