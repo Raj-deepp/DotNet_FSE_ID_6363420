02e7568ed65fd9edfc101402dc950c2fd1ff00ad
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-22 00:28:52 +0530

    Invoke CalculateScore func

--- Week_6/3_ReactJS-HOL/scorecalculatorapp/src/Components/CalculateScore.js
+++ Week_6/3_ReactJS-HOL/scorecalculatorapp/src/Components/CalculateScore.js
@@ -13,23 +13,23 @@ export const CalculateScore = ({Name,School,total,goal}) => (
     <h1 style={{ color: 'brown' }}>Student Details:</h1>
 
     <div className="Name">
-      <b><span>Name:</span></b>
+      <b><span>Name: </span></b>
       <span>{Name}</span>
     </div>
 
     <div className="School">
-      <b><span>School:</span></b>
+      <b><span>School: </span></b>
       <span>{School}</span>
     </div>
 
     <div className="Total">
-      <b><span>Total:</span></b>
+      <b><span>Total: </span></b>
       <span>{total}</span>
       <span>Marks</span>
     </div>
 
     <div className="Score">
-      <b>Score:</b>
+      <b>Score: </b>
       <span>{calcScore(total,goal)}</span>
     </div>
   </div>
