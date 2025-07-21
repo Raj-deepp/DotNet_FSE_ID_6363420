59f1678851a81594694929919257ed7d7d3f80d7
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-21 19:33:05 +0530

    myFirstReact

---
+++ Week_6/1_ReactJS-HOL/myfirstreact/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+      <h1>Welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
