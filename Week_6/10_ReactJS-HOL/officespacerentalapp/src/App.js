3ae124050a4f77f4b3d58342a6ae73a0120a02e0
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-07-29 01:05:26 +0530

    office list

---
+++ Week_6/10_ReactJS-HOL/officespacerentalapp/src/App.js
@@ -0,0 +1,26 @@
+function App() {
+  const heading = "Office Space at Affordable Range";
+
+  const officeList = [
+    {
+      name: 'DBS',
+      rent: 50000,
+      address: 'Chennai',
+      image: 'https://officebanao.com/wp-content/uploads/2024/06/office-with-lot-desks-computers.jpg'
+    },
+    {
+      name: 'EcoOffice',
+      rent: 65000,
+      address: 'Kolkata',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZPFv3KqBOfVxnr2C_vVoqNMMlLYgM_DUcgA&s'
+    },
+    {
+      name: 'WorkNest',
+      rent: 46000,
+      address: 'Bangalore',
+      image: 'https://media.istockphoto.com/id/1141421616/photo/this-office-space-is-filled-with-productivity.jpg?s=612x612&w=0&k=20&c=ka6rGPcVkhjC2cG-ICEQaE7mzufJ253bngJPahvBlCg='
+    }
+  ];
+}
+
+export default App;
