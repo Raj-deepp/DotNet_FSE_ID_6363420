97a86004a94fb765c6e3a3b080852620843771cf
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-08-01 23:56:01 +0530

    basic styling

--- Week_7/12_ReactJS-HOL/ticketbookingapp/src/User.js
+++ Week_7/12_ReactJS-HOL/ticketbookingapp/src/User.js
@@ -3,7 +3,7 @@ import FlightList from './FlightList';
 function User(){
   return (
     <div>
-      <h2>Welcome User!</h2>
+      <h2 >Welcome User!</h2>
       <p>You can now book your flight.</p>
       <FlightList showBooking={true} />
     </div>
