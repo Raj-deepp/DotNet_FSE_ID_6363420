740dc42e8935957b525a75dcaa339ad4f60642b5
Rajdeep Mandal <mrajdeep651@gmail.com>
2025-08-02 20:09:20 +0530

    COmponents updated

---
+++ Week_7/13_ReactJS-HOL/bloggerapp/src/components/BlogDetails.js
@@ -0,0 +1,12 @@
+function BlogDetails() {
+  return (
+    <div>
+      <h2>Blog Details</h2>
+      <p>Title: Mastering React Hooks</p>
+      <p>Author: Jane Doe</p>
+      <p>Date: Aug 2025</p>
+    </div>
+  );
+}
+
+export default BlogDetails;
---
+++ Week_7/13_ReactJS-HOL/bloggerapp/src/components/CourseDetails.js
@@ -0,0 +1,12 @@
+function CourseDetails() {
+  return (
+    <div>
+      <h2>Course Details</h2>
+      <p>Name: React - The Complete Guide</p>
+      <p>Instructor: Max Schwarzmüller</p>
+      <p>Duration: 40 hours</p>
+    </div>
+  );
+}
+
+export default CourseDetails;
